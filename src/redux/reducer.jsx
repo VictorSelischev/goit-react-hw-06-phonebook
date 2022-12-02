caee4bd4d5fed91514e64a9fa32bb0ada7f0c1d1
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2022-12-02 19:12:14 +0200

    Create store

---
+++ src/redux/reducer.jsx
@@ -0,0 +1,8 @@
+const initialState = {
+  contacts: [],
+  filter: ""
+}
+
+export const rootReducer = (state = initialState, action) => {
+  return state
+}
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,17 +3,12 @@
 // import { filterReducer } from './filterSlice';
 
 import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+import { rootReducer } from './reducer';
 
-const initialState = {
-  contacts: [],
-  filter: ""
-}
+const enhancer = devToolsEnhancer();
 
-const rootReducer = (state = initialState, action) => {
-  return state
-}
-
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, enhancer);
 
 
 
