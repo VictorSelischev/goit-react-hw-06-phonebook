76974e272ddee0a20c9644a2046b5eaddbdcba37
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2022-12-02 12:55:49 +0200

    Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
-import { filterReducer } from './filterSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsReducer } from './contactsSlice';
+// import { filterReducer } from './filterSlice';
 
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
-});
+import { createStore } from 'redux';
+
+const initialState = {
+  contacts: [],
+  filter: ""
+}
+
+const rootReducer = (state = initialState, action) => {
+  return state
+}
+
+export const store = createStore(rootReducer);
+
+
+
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+// });
