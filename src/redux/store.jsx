1bb515e4bbea9af1f46b85d2b77526f4d3b2afe6
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-05 21:13:55 +0200

    create devToolsEnhancer

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 
 const initialState = {
     contacts: [
@@ -11,5 +12,6 @@ const initialState = {
 }
 
 const rootReducer = (state = initialState, action) => { return state; };
+const enhancer = devToolsEnhancer();
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, enhancer);
