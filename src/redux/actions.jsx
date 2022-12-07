2abce621d2f5a50d9e4de037c357f61b7adc9438
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-07 22:25:47 +0200

    Actions-gerenerators

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -4,23 +4,46 @@ import { nanoid } from 'nanoid';
 - Удалить контакт из телефонной книги;
 - Обновить значение фильтра телефонной книги.
 
-const addContact = {
-    type: 'contacts/addContact',
-    payload: {
-        id: nanoid(),
-        name: 'User name',
-        number: 'Phone number',
-    }
-}
+// const addContact = {
+//     type: 'contacts/addContact',
+//     payload: {
+//         id: nanoid(),
+//         name: 'User name',
+//         number: 'Phone number',
+//     }
+// }
 
-const deleteContact = {
-    type: 'contacts/deleteContact',
-    payload: 'Contact Id',
-}
+// const deleteContact = {
+//     type: 'contacts/deleteContact',
+//     payload: 'Contact Id',
+// };
 
-const setValueFilter = {
-    type: 'filter/setValueFilter',
-    payload: 'Filter value',
-}
+// const setValueFilter = {
+//     type: 'filter/setValueFilter',
+//     payload: 'Filter value',
+// };
 
+export const addContact = (name, number) => {
+    return {
+        type: 'contacts/addContact',
+        payload: {
+            id: nanoid(),
+            name,
+            number,
+        },
+    };
+};
 
+export const deleteContact = contactId => {
+    return {
+        type: 'contacts/deleteContact',
+        payload: contactId,
+    };
+};
+
+export const setValueFilter = value => {
+    return {
+        type: 'filter/setValueFilter',
+        payload: value,
+    };
+};
