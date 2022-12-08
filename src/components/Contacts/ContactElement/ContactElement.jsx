483e07249cbd54f386176e5e2f26f6171b3274b6
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-08 15:14:49 +0200

    ContactElement.propTypes

--- src/components/Contacts/ContactElement/ContactElement.jsx
+++ src/components/Contacts/ContactElement/ContactElement.jsx
@@ -1,6 +1,7 @@
 import css from './ContactElement.module.css';
 import PropTypes from 'prop-types';
 
+
   // const deleteContact = contactId => {
   //   const newArray = contacts.filter(contact => contact.id !== contactId);
   //   setContacts(newArray);
@@ -20,7 +21,10 @@ export const ContactElement = ({contact}) => {
 };
 
 ContactElement.propTypes = {
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }))
 }
