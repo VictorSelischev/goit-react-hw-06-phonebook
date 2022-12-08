3242c760fb2acbe20723368c83f0efaf2c6836e1
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-08 08:05:55 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,9 +1,14 @@
 import css from './Filter.module.css';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getFilter } from 'redux/selectors';
+import { setValueFilter } from 'redux/actions';
+
+const handleChange = e => {};
 
 export const Filter = () => {
   const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
   console.log(filter);
   return (
     <label className={css.formAddBook__label}>
@@ -11,7 +16,9 @@ export const Filter = () => {
       <input
         className={css.formAddBook__input}
         type="text"
-        name='filter'
+        name="filter"
+        value={dispatch(setValueFilter(value))}
+        onChange={handleChange}
       />
     </label>
   );
