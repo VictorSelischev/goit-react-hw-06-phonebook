42d9d2ce4a058dd91332ac8f941783eb19087169
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-08 16:55:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,7 @@ import { Section } from './Section/Section';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsList } from './Contacts/ContactsList';
 import { Filter } from './Filter/Filter';
-// import { useState, useEffect } from 'react';
 // import { save, load } from '../services/utilitsLocalStorage';
-
-//     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ]
 
 export const App = () => {
   // const CONTACTS_KEY = 'contacts';
@@ -23,32 +15,6 @@ export const App = () => {
   // useEffect(() => {
   //   save(CONTACTS_KEY, contacts);
   // }, [contacts]);
-
-  // const addContact = data => {
-  //   contacts.find(
-  //     contact => data.name.toLowerCase() === contact.name.toLocaleLowerCase()
-  //   )
-      // ? alert(`${data.name} is already in contacts`)
-  //     : setContacts(contacts => [data, ...contacts]);
-  // };
-
-  // const deleteContact = contactId => {
-  //   const newArray = contacts.filter(contact => contact.id !== contactId);
-  //   setContacts(newArray);
-  // };
-
-  // const handleChangeFilter = evt => {
-  //   setFilter(evt.target.value);
-  // };
-
-  // const getVisibleContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-
-  // const visibleContacts = getVisibleContacts();
 
   return (
     <div
@@ -68,10 +34,7 @@ export const App = () => {
       </Section>
       <Section title={'Contacts'}>
         <Filter />
-        <ContactsList
-          // contacts={visibleContacts}
-          // onDeleteContact={deleteContact}
-        />
+        <ContactsList />
       </Section>
     </div>
   );
