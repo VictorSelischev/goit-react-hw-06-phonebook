11ef8057b7e52265b84f2b1039555c5678783d05
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-08 16:08:13 +0200

    reducer deleteContact

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -11,7 +11,14 @@ const initialState = {
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'contacts/addContact':
-      return { ...state, contacts: [...state.contacts, action.payload,], }
+      return { ...state, contacts: [...state.contacts, action.payload] };
+    case 'contacts/deleteContact':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
