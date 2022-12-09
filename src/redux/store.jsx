e2fa3e286bd680a262e304e7157ea44601f582a1
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-09 13:54:57 +0200

    create configureStore

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducer';
 
-const enhancer = devToolsEnhancer();
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
 
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+    reducer: rootReducer,
+});
