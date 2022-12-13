3e68b91fed04c1b616902fccc7c6513e3ad01fa1
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2022-12-13 18:35:54 +0200

    Update filterSlice.jsx

--- src/redux/filterSlice.jsx
+++ src/redux/filterSlice.jsx
@@ -1,17 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const filterInitialState = '';
 
 const filterSlice = createSlice({
-    name: 'filter',
-    initialState: filterInitialState,
-    reducers: {
-        filterReducer(state, action) {
-            return action.payload;
-        }
-    }
+  name: 'filter',
+  initialState: filterInitialState,
+  reducers: {
+    filterReducer(state, action) {
+      state.filter = action.payload;
+    },
+  },
 });
 
-export const {setValueFilter} = filterSlice.actions;
+export const { setValueFilter } = filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
